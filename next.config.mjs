b0fe1d697c16d24cc0106e6d7276222d921e3426
Verno01 <235706149+Verno01@users.noreply.github.com>
2026-05-20 12:22:07 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { formats: ['image/avif', 'image/webp'] },
-}
-export default nextConfig
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
